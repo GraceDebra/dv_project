0bb951a0af5390f1ab43064a1a2ebc12acda0245
Fadumo111 <musa.memusi@students.jkuat.ac.ke>
2025-05-17 17:22:01 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ import Report from "./Report"
 import Support from "./Support"
 import RiskAssessment from "./RiskAssessment"
 import Admin from "./Admin"
-import AISupport from "./AISupport"
 import Chatbot from "./Chatbot"
 
 const Home = () => {
@@ -256,7 +255,7 @@ const App = () => {
         <Route path="/testimonials" element={<Testimonials />} />
         <Route path="/report-incident" element={<Report/>} />
         <Route path="/support" element={<Support />} />
-        {/*<Route path="/chatbot" element={<AISupport />} /> */}
+        <Route path="/admin" element={<Admin />} />
         <Route path="/chatbot" element={<Chatbot />} />
         <Route path="/risk-assessment" element={<RiskAssessment />} />
 
